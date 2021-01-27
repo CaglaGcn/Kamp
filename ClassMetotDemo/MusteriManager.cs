3dc39b40a3478a7cc092c88b94eefac12e0070c9
CaglaGcn <cgla.glcn@hotmail.com>
2021-01-27 20:33:27 +0300

    3. ders sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " " + " isimli müşteri eklenmiştir.");
+
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " " + " isimli müşteri silinmiştir.");
+        }
+
+    }
+}
+   
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Adi = "Ali";
+            string Soyadi = "Solak";
+            int Yasi = 19;
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "Solak";
+            musteri1.Yasi = 19;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Kemal";
+            musteri2.Soyadi = "Topal";
+            musteri2.Yasi = 23;
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri Musteri in musteri)
+            {
+                Console.WriteLine(musteri1.Adi);
+                Console.WriteLine(musteri1.Soyadi);
+                Console.WriteLine(musteri1.Yasi);
+                Console.WriteLine("----");
+
+                Console.WriteLine(musteri2.Adi);
+                Console.WriteLine(musteri2.Soyadi);
+                Console.WriteLine(musteri2.Yasi);
+                Console.WriteLine("----");
+
+            }
+
+        }
+}
+}
